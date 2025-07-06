c19aefb6b480467a1d29c13ba0afae2cb65eb352
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2025-07-06 21:07:11 +0300

    created test_succesful_registration.py and fixed registration_page.py

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,5 +1,15 @@
-from selenium.webdriver.common.by import By
+import time
 
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def scroll_and_wait_clickable(driver, locator):
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located(locator))
+    driver.execute_script("arguments[0].scrollIntoView(true);", element)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(locator))
+    return element
 class RegistrationPage:
     def __init__(self, driver):
         self.driver = driver
@@ -10,12 +20,13 @@ class RegistrationPage:
         self.female_radio = (By.ID, "gender-radio-2")
         self.mobile_nr_field = (By.ID, "userNumber")
         self.calendar_field = (By.ID, "dateOfBirthInput")
-        self.subject_field = (By.ID, "subjectsContainer")
+        self.subject_field = (By.ID, "subjectsInput")
         self.hobby_checkboxes = {
-            "Sports": (By.ID, "hobby-checkbox-1"),
-            "Reading": (By.ID, "hobby-checkbox-2"),
-            "Music": (By.ID, "hobby-checkbox-3")
+            "Sports": (By.XPATH, "//label[@for='hobbies-checkbox-1']"),
+            "Reading": (By.XPATH, "//label[@for='hobbies-checkbox-2']"),
+            "Music": (By.XPATH, "//label[@for='hobbies-checkbox-3']")
         }
+
         self.upload_pic_button = (By.ID, "uploadPicture")
         self.current_address_field = (By.ID, "currentAddress")
         self.state_choice = (By.ID, "states")
@@ -36,26 +47,32 @@ class RegistrationPage:
 
     def choose_gender_ratio(self, gender):
         if gender == "Male":
-            radio = self.driver.find_element(*self.male_radio)
+            label = self.driver.find_element(By.XPATH, "//label[@for='gender-radio-1']")
         else:
-            radio = self.driver.find_element(*self.female_radio).click()
+            label = self.driver.find_element(By.XPATH, "//label[@for='gender-radio-2']")
 
-        if not radio.is_selected():
-            radio.click()
+        label.click()
 
     def enter_phone_number(self, mobile_number):
         self.driver.find_element(*self.mobile_nr_field).send_keys(mobile_number)
 
     def choose_date(self, date):
-        self.driver.find_element(*self.calendar_field).send_keys(date)
+        date_input = self.driver.find_element(*self.calendar_field)
+        date_input.click()
+        #date_input.send_keys(Keys.CONTROL + "a") # pažymi visą datą
+        date_input.send_keys(date)
+        date_input.send_keys(Keys.ENTER) # svarbu! uždaro date picker
+
 
     def enter_subject(self, subject):
-        self.driver.find_element(*self.subject_field).send_keys(subject)
-
+        subject_input = self.driver.find_element(*self.subject_field)
+        subject_input.send_keys(subject)
+        subject_input.send_keys(Keys.ENTER)
     def choose_hobby(self, hobby_name):
         locator = self.hobby_checkboxes.get(hobby_name) # lygu locator = (By.ID, "hobby-checkbox-1")
         if locator:
-            checkbox = self.driver.find_element(*locator)
+            wait = WebDriverWait(self.driver, 10)
+            checkbox = wait.until(EC.element_to_be_clickable(locator))
             if not checkbox.is_selected():
                 checkbox.click()
 
@@ -66,13 +83,22 @@ class RegistrationPage:
         self.driver.find_element(*self.current_address_field).send_keys(current_address)
 
     def choose_state_by_visible_text(self, state):
-        self.driver.find_element(*self.state_choice).select_by_visible_text(state)
-
+        state_dropdown_locator = (By.ID, "state")
+        state_dropdown = scroll_and_wait_clickable(self.driver, state_dropdown_locator)
+        state_dropdown.click()
+        option = self.driver.find_element(By.XPATH,
+                                          f"//div[contains(@id, 'react-select-3-option') and text()='{state}']")
+        option.click()
     def choose_city_by_visible_text(self, city):
-        self.driver.find_element(*self.city_choice).select_by_visible_text(city)
+        city_dropdown = self.driver.find_element(By.ID, "city")
+        city_dropdown.click()
+        time.sleep(1)
+        city_input = self.driver.find_element(By.ID, "react-select-4-input")
+        city_input.send_keys(city)
+        city_input.send_keys(Keys.ENTER)
 
-    def click_submit(self, submit_button):
-        self.driver.find_element(*self.submit_button).select_by_visible_text(submit_button)
+    def click_submit(self):
+        self.driver.find_element(*self.submit_button).click()
 
     def is_result_table_displayed(self):
         return self.driver.find_element(By.ID, "example-modal-sizes-title-lg").is_displayed()
--- tests/test_succesful_registration.py
+++ tests/test_succesful_registration.py
@@ -1,6 +1,6 @@
 from pages.registration_page import RegistrationPage
 
-def valid_registration(driver):
+def test_valid_registration(driver):
     registration_page = RegistrationPage(driver)
     registration_page.open("https://demoqa.com/automation-practice-form")
 
@@ -8,11 +8,11 @@ def valid_registration(driver):
     registration_page.enter_last_name("Gelezevicius")
     registration_page.enter_email("andrius.gelezevicius@example.com")
     registration_page.choose_gender_ratio("Male")
-    registration_page.enter_phone_number("123456789")
+    registration_page.enter_phone_number("1234567890")
     registration_page.choose_date("10 February 1989")
     registration_page.enter_subject("Maths")
     registration_page.choose_hobby("Sports")
-    registration_page.upload_photo("C:/Users/PC/Desktop/ProfilePhoto.png")
+    registration_page.upload_photo("C:/Users/PC/Desktop/ProfilePhoto.jpg")
     registration_page.enter_current_adress("123 Test street")
     registration_page.choose_state_by_visible_text("NCR")
     registration_page.choose_city_by_visible_text("Delhi")
