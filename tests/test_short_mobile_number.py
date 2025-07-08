613738b020cac69fbbb68d9c427c36df3f730278
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2025-07-08 22:12:01 +0300

    fixed as in case and created test_short_mobile_number.py

--- tests/test_short_mobile_number.py
+++ tests/test_short_mobile_number.py
@@ -2,15 +2,15 @@ from selenium.webdriver.common.by import By
 
 from pages.registration_page import RegistrationPage
 
-def test_wrong_email_format(driver):
+def test_short_mobile_number(driver):
     registration_page = RegistrationPage(driver)
     registration_page.open("https://demoqa.com/automation-practice-form")
 
     registration_page.enter_first_name("Andrius")
     registration_page.enter_last_name("Gelezevicius")
-    registration_page.enter_email("andriius.gel.lt")
+    registration_page.enter_email("andrius.gel@example.com")
     registration_page.choose_gender_ratio("Male")
-    registration_page.enter_phone_number("andriius.gel.lt")
+    registration_page.enter_phone_number("123")
     registration_page.choose_date("10 February 1989")
     registration_page.enter_subject("Maths")
     registration_page.choose_hobby("Sports")
@@ -20,8 +20,11 @@ def test_wrong_email_format(driver):
     registration_page.choose_city_by_visible_text("Delhi")
     registration_page.click_submit()
 
-    email_element = driver.find_element(By.ID, "userEmail")
-    border_color_email = email_element.value_of_css_property("border-color")
-    assert "rgb(220, 53, 69)" in border_color_email, f"Expected red border, got {border_color_email}"
+    # Checks if mobile number border became red
+    mobile_number_element = driver.find_element(By.ID, "userNumber")
+    border_color_mobile = mobile_number_element.value_of_css_property("border-color")
+    assert "rgb(220, 53, 69)" in border_color_mobile, "Expected red border for short password"
 
+    # After click submit result model doesn't shows
     assert not registration_page.is_result_table_displayed(), "Result modal should not be displayed"
+
